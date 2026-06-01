495ba7cd0f56ac1708e8520ac821d22b26764bb4
Deyvi Lizandro Rivera <deyvi2413@gmail.com>
2026-06-01 08:53:55 -0500

    Clase 1/06/2026

--- Modulo_3/blog-backend/src/app.module.ts
+++ Modulo_3/blog-backend/src/app.module.ts
@@ -7,10 +7,12 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { CategoriesModule } from './categories/categories.module';
 import { PostsModule } from './posts/posts.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URI || ''),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
