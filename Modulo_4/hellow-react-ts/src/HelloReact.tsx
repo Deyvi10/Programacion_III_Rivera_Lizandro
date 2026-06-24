7938defe31152f44fe9ce2d93c65333540f53949
Deyvi10 <deyvi2413@gmail.com>
2026-06-24 07:09:58 -0500

    Clase 22/06/2026

--- Modulo_4/hellow-react-ts/src/HelloReact.tsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <section style={{ maxWidth: 600, 
-    margin: '40px auto', fontFamily: 'sans-serif' }}>  
-      <h1 style={{color: 'white'}}>Hola desde React 19 + TypeScript</h1>
-      <p>Version: 1.0.0</p>
-    </section>
-  )
-}
